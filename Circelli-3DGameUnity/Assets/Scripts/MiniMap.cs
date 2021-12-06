1ef62c8fe57c99b82f8ae31fc45413d0d324d93a
rhino2199 <rcircelli21@gmail.com>
2021-12-06 11:48:54 -0600

    New Rain Video
    New Rain Video
    MiniMap Scripts condensed

--- Circelli-3DGameUnity/Assets/Scripts/MiniMap.cs
+++ Circelli-3DGameUnity/Assets/Scripts/MiniMap.cs
@@ -14,11 +14,11 @@ public class MiniMap : MonoBehaviour
 {
 
     public Transform Player;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject Floor1;
+    public GameObject Floor2;
+    public GameObject Floor3;
+    public GameObject Floor4;
+
 
     // Update is called once per frame
     void LateUpdate()
@@ -26,5 +26,37 @@ public class MiniMap : MonoBehaviour
         Vector3 newPos = Player.position;
         newPos.y = transform.position.y;
         transform.position = newPos;
+    }
+
+    void Update()
+    {
+        if (Player.position.y < 1)
+        {
+            Floor1.SetActive(true);
+            Floor2.SetActive(false);
+            Floor3.SetActive(false);
+            Floor4.SetActive(false);
+        }
+        else if (Player.position.y > 1 && Player.position.y < 5)
+        {
+            Floor1.SetActive(false);
+            Floor2.SetActive(true);
+            Floor3.SetActive(false);
+            Floor4.SetActive(false);
+        }
+        else if (Player.position.y > 5 && Player.position.y < 9)
+        {
+            Floor1.SetActive(false);
+            Floor2.SetActive(false);
+            Floor3.SetActive(true);
+            Floor4.SetActive(false);
+        }
+        else if (Player.position.y > 9)
+        {
+            Floor1.SetActive(false);
+            Floor2.SetActive(false);
+            Floor3.SetActive(false);
+            Floor4.SetActive(true);
+        }
     }
 }
